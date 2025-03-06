bd45e710357d3142179e61114e6552c3a8f89e79
bybeejac000 <jake.bybee@bybeemail.com>
2025-03-05 17:51:11 -0700

    commetns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import data from './CollegeBasketballTeams.json';
 
-var team = data.teams[0];
+//Make the data an object
 var teams = data.teams;
 
+//Header
 function Heading() {
   return <h1>Welcome to the teams page!</h1>;
 }
 
+//Make the card template
 function TeamCard({
   name,
   mascot,
@@ -32,12 +34,14 @@ function TeamCard({
   );
 }
 
+//iterate over the rows
 function BigData() {
   return teams.map((r) => (
     <TeamCard name={r.school} mascot={r.name} city={r.city} state={r.state} />
   ));
 }
 
+//send it all to the app
 function App() {
   return (
     <>
